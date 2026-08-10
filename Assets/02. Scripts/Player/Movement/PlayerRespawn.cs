3d9fa4e632880c552b8dcde6cab1f316b64d7023
kms1598 <kms7231931@naver.com>
2026-08-10 12:25:50 +0900

    카메라 수정

--- Assets/02. Scripts/Player/Movement/PlayerRespawn.cs
+++ Assets/02. Scripts/Player/Movement/PlayerRespawn.cs
@@ -1,8 +1,11 @@
+using System;
 using UnityEngine;
 
 public class PlayerRespawn : MonoBehaviour
 {
     private Vector3 currentSpawnPos;
+
+    public static event Action OnRespawn;
 
     void Awake()
     {
@@ -20,5 +23,7 @@ public class PlayerRespawn : MonoBehaviour
         transform.position = currentSpawnPos;
         PlayerController.instance.cc.enabled = true;
         PlayerController.instance.ResetVelocity();
+
+        OnRespawn?.Invoke();
     }
 }
